bb152abdf5bc9ea06b7dd8397db55d1360699f84
Zain Ahmed <zaahm18@student.sdu.dk>
2022-04-27 14:38:48 +0200

    added more inputs and outputs

--- Controller.py
+++ Controller.py
@@ -11,6 +11,7 @@ class DroneController:
     def Setup(self):
         self.SetController()
         self.SetInputs()
+        self.SetOutputs()
         
         
     def SetController(self):
@@ -62,5 +63,40 @@ class DroneController:
         lv_Theta = sf.LinguisticVariable(Input_Theta, concept = "Difference in marker angle in yaw/theta axis", universe_of_discourse = [-70, 70])
         self.GetController().add_linguistic_variable("theta", lv_Theta, verbose = True)
 
+    def SetOutputs(self):       
+       Outputs_X = []
+       Outputs_X.append(sf.FuzzySet(points=[[-1.0, 1.0], [-0.8, 0.0]], term = "hard left", verbose = True))
+       Outputs_X.append(sf.FuzzySet(function=sf.Triangular_MF(-1.0, -0.7, -0.4), term = "left", verbose = True))
+       Outputs_X.append(sf.FuzzySet(function=sf.Triangular_MF(-0.6, -0.3, -0.0), term = "soft left", verbose = True))
+       Outputs_X.append(sf.FuzzySet(function=sf.Gaussian_MF(0, 0.1), term = "stop", verbose = True))
+       Outputs_X.append(sf.FuzzySet(function=sf.Triangular_MF(0.0, 0.3, 0.6), term = "soft right", verbose = True))
+       Outputs_X.append(sf.FuzzySet(function=sf.Triangular_MF(0.4, 0.7, 1.0), term = "right", verbose = True))
+       Outputs_X.append(sf.FuzzySet(points=[[0.8, 0.0], [1.0, 1.0]], term = "hard right", verbose = True))
+       lv_X = sf.LinguisticVariable(Outputs_X, concept = "Speed in lateral(x) axis", universe_of_discourse = [-1.0, 1.0])
+       self.GetController().add_linguistic_variable("speed_x", lv_X, verbose = True)
+       
+       Outputs_Y = []
+       Outputs_Y.append(sf.FuzzySet(points=[[-1.0, 1.0], [-0.8, 0.0]], term = "ascend hard", verbose = True))
+       Outputs_Y.append(sf.FuzzySet(function=sf.Triangular_MF(-1.0, -0.7, -0.4), term = "ascend", verbose = True))
+       Outputs_Y.append(sf.FuzzySet(function=sf.Triangular_MF(-0.6, -0.3, -0.0), term = "ascend light", verbose = True))
+       Outputs_Y.append(sf.FuzzySet(function=sf.Gaussian_MF(0, 0.1), term = "hover", verbose = True))
+       Outputs_Y.append(sf.FuzzySet(function=sf.Triangular_MF(0.0, 0.3, 0.6), term = "descend light", verbose = True))
+       Outputs_Y.append(sf.FuzzySet(function=sf.Triangular_MF(0.4, 0.7, 1.0), term = "descend", verbose = True))
+       Outputs_Y.append(sf.FuzzySet(points=[[0.8, 0.0], [1.0, 1.0]], term = "descend hard", verbose = True))
+       lv_Y = sf.LinguisticVariable(Outputs_Y, concept = "Speed in vertical(y) axis", universe_of_discourse = [-1.0, 1.0])
+       self.GetController().add_linguistic_variable("speed_y", lv_Y, verbose = True)
+    
+    
+        Outputs_Y = []
+       Outputs_Y.append(sf.FuzzySet(points=[[-1.0, 1.0], [-0.8, 0.0]], term = "ascend hard", verbose = True))
+       Outputs_Y.append(sf.FuzzySet(function=sf.Triangular_MF(-1.0, -0.7, -0.4), term = "ascend", verbose = True))
+       Outputs_Y.append(sf.FuzzySet(function=sf.Triangular_MF(-0.6, -0.3, -0.0), term = "ascend light", verbose = True))
+       Outputs_Y.append(sf.FuzzySet(function=sf.Gaussian_MF(0, 0.1), term = "hover", verbose = True))
+       Outputs_Y.append(sf.FuzzySet(function=sf.Triangular_MF(0.0, 0.3, 0.6), term = "descend light", verbose = True))
+       Outputs_Y.append(sf.FuzzySet(function=sf.Triangular_MF(0.4, 0.7, 1.0), term = "descend", verbose = True))
+       Outputs_Y.append(sf.FuzzySet(points=[[0.8, 0.0], [1.0, 1.0]], term = "descend hard", verbose = True))
+       lv_Y = sf.LinguisticVariable(Outputs_Y, concept = "Speed in vertical(y) axis", universe_of_discourse = [-1.0, 1.0])
+       self.GetController().add_linguistic_variable("speed_y", lv_Y, verbose = True)
+    
     def GetController(self) -> sf.FuzzySystem:
         return self.controller
